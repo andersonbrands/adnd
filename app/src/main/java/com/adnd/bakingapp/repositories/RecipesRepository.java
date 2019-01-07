6ad6ad9a3fd685674475c6ec58a8815d79dd9370
Anderson Brandao <anderson.brands@gmail.com>
2019-01-07 16:48:52 -0200

    feat: Add Dao's to Recipe repository
     - Save recipe to database
     - Load recipe from database

--- app/src/main/java/com/adnd/bakingapp/repositories/RecipesRepository.java
+++ app/src/main/java/com/adnd/bakingapp/repositories/RecipesRepository.java
@@ -1,8 +1,16 @@
 package com.adnd.bakingapp.repositories;
 
+import android.app.Application;
+
 import com.adnd.bakingapp.api.RecipesApi;
 import com.adnd.bakingapp.api.RecipesListConverterFactory;
+import com.adnd.bakingapp.database.AppDatabase;
+import com.adnd.bakingapp.database.IngredientsDao;
+import com.adnd.bakingapp.database.RecipesDao;
+import com.adnd.bakingapp.database.StepsDao;
+import com.adnd.bakingapp.models.Ingredient;
 import com.adnd.bakingapp.models.Recipe;
+import com.adnd.bakingapp.models.Step;
 
 import java.util.List;
 
@@ -14,6 +22,39 @@ import retrofit2.Retrofit;
 public class RecipesRepository {
 
     private static final String TAG = RecipesRepository.class.getSimpleName();
+
+    private RecipesDao recipesDao;
+    private IngredientsDao ingredientsDao;
+    private StepsDao stepsDao;
+
+    public RecipesRepository(Application application) {
+        AppDatabase db = AppDatabase.getInstance(application);
+
+        recipesDao = db.recipesDao();
+        ingredientsDao = db.ingredientsDao();
+        stepsDao = db.stepsDao();
+    }
+
+    public void saveRecipeToDatabase(Recipe recipe) {
+        recipesDao.insert(recipe);
+        for (Ingredient ingredient : recipe.getIngredients()) {
+            ingredient.setId(recipe.getId());
+            ingredientsDao.insert(ingredient);
+        }
+        for (Step step : recipe.getSteps()) {
+            step.setId(recipe.getId());
+            stepsDao.insert(step);
+        }
+    }
+
+    public List<Recipe> loadRecipesFromDatabase() {
+        List<Recipe> recipes = recipesDao.getRecipes();
+        for (Recipe recipe: recipes) {
+            recipe.setIngredients(ingredientsDao.getIngredientsForRecipe(recipe.getId()));
+            recipe.setSteps(stepsDao.getStepsForRecipe(recipe.getId()));
+        }
+        return recipes;
+    }
 
     public void loadRecipesFromWebService() {
         Retrofit fit = new Retrofit.Builder()
