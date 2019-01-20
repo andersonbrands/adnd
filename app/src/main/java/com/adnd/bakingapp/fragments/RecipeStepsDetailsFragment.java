b39a3be1e547b18d735297f79e0a1fb56676f968
Anderson Brandao <anderson.brands@gmail.com>
2019-01-19 22:13:51 -0200

    feat: Remove ViewPager

--- app/src/main/java/com/adnd/bakingapp/fragments/RecipeStepsDetailsFragment.java
+++ app/src/main/java/com/adnd/bakingapp/fragments/RecipeStepsDetailsFragment.java
@@ -6,26 +6,15 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentStatePagerAdapter;
-import android.support.v4.view.ViewPager;
-import android.support.v7.widget.LinearLayoutManager;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TableLayout;
 
-import com.adnd.bakingapp.adapters.RecipeStepAdapter;
-import com.adnd.bakingapp.adapters.RecipeStepsPagerAdapter;
 import com.adnd.bakingapp.databinding.FragmentRecipeStepDetailsBinding;
 import com.adnd.bakingapp.models.Recipe;
 import com.adnd.bakingapp.view_models.RecipeDetailsActivityViewModel;
 
 public class RecipeStepsDetailsFragment extends Fragment {
-
-    private RecipeStepsPagerAdapter adapter;
-
-    private PageChangeListener pageChangeListener = new PageChangeListener();
 
     public RecipeStepsDetailsFragment() {
 
@@ -42,17 +31,14 @@ public class RecipeStepsDetailsFragment extends Fragment {
                 public void onChanged(@Nullable Recipe recipe) {
                     if (recipe != null) {
                         binding.setRecipe(recipe);
-                        adapter = new RecipeStepsPagerAdapter(getFragmentManager(), recipe.getSteps(), getResources());
-                        binding.vpRecipeSteps.setAdapter(adapter);
-                        binding.vpRecipeSteps.addOnPageChangeListener(pageChangeListener);
                     }
                 }
             });
             model.getSelectedStepPosition().observe(this, new Observer<Integer>() {
                 @Override
                 public void onChanged(@Nullable Integer position) {
-                    if (adapter != null && position != null) {
-                        binding.vpRecipeSteps.setCurrentItem(position);
+                    if (position != null) {
+                        binding.setSelectedStepPosition(position);
                     }
                 }
             });
@@ -60,38 +46,4 @@ public class RecipeStepsDetailsFragment extends Fragment {
         return binding.getRoot();
     }
 
-    public interface iPauseResume {
-
-        void onPauseFragment();
-
-        void onResumeFragment();
-
-    }
-
-    private class PageChangeListener implements ViewPager.OnPageChangeListener {
-
-        int currentPosition = 0;
-
-        @Override
-        public void onPageSelected(int position) {
-//            if (currentPosition != -1) {
-                iPauseResume fragmentToHide = (iPauseResume) adapter.getItem(currentPosition);
-                fragmentToHide.onPauseFragment();
-//            }
-
-            iPauseResume fragmentToShow = (iPauseResume) adapter.getItem(position);
-            fragmentToShow.onResumeFragment();
-
-            currentPosition = position;
-            Log.d("TEMP_TEG", "onPageSelected(int position): " + position);
-        }
-
-        @Override
-        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-        }
-
-        public void onPageScrollStateChanged(int state) {
-        }
-
-    }
 }
