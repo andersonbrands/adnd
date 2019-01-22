9ff8ddb24e9d3bf11cb575f88e81b7be5a6407f9
Anderson Brandao <anderson.brands@gmail.com>
2019-01-22 20:23:45 -0200

    test: Add a TestSuite class for running all tests in one go

---
+++ app/src/androidTest/java/com/adnd/bakingapp/TestSuite.java
@@ -0,0 +1,14 @@
+package com.adnd.bakingapp;
+
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        MainActivityTest.class,
+        RecipeDetailsActivityTest.class
+})
+
+public class TestSuite {
+}
