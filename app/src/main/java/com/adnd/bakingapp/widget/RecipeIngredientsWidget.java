2b31e810de48cf1c30b2c49c13cb5cf979def7ec
Anderson Brandao <anderson.brands@gmail.com>
2019-01-24 21:31:52 -0200

    refactor: Moving widget provider to widget package

--- app/src/main/java/com/adnd/bakingapp/widget/RecipeIngredientsWidget.java
+++ app/src/main/java/com/adnd/bakingapp/widget/RecipeIngredientsWidget.java
@@ -1,4 +1,4 @@
-package com.adnd.bakingapp;
+package com.adnd.bakingapp.widget;
 
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
@@ -6,6 +6,9 @@ import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
 import android.widget.RemoteViews;
+
+import com.adnd.bakingapp.MainActivity;
+import com.adnd.bakingapp.R;
 
 /**
  * Implementation of App Widget functionality.
