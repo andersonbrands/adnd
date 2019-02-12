3c843bf537f1e61340105f6a9f6441e82d3a31d4
Anderson Brandao <anderson.brands@gmail.com>
2019-02-12 18:41:38 -0200

    feat: Move Coordinator layout to details activity layout

--- app/src/main/java/com/adnd/xyzreader/ArticleDetailsActivity.java
+++ app/src/main/java/com/adnd/xyzreader/ArticleDetailsActivity.java
@@ -1,20 +1,27 @@
 package com.adnd.xyzreader;
 
+import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.adnd.xyzreader.databinding.ArticleDetailsActivityBinding;
+import com.adnd.xyzreader.models.Article;
 import com.adnd.xyzreader.view_models.ArticleDetailsViewModel;
 
 public class ArticleDetailsActivity extends AppCompatActivity {
 
     public static final String ARTICLE_ID_EXTRA_KEY = "article_id_extra_key";
 
+    ArticleDetailsActivityBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.article_details_activity);
+        binding = DataBindingUtil.setContentView(this, R.layout.article_details_activity);
 
         Intent intentThatStartedThisActivity = getIntent();
 
@@ -25,6 +32,15 @@ public class ArticleDetailsActivity extends AppCompatActivity {
             int articleId =
                     intentThatStartedThisActivity.getIntExtra(ARTICLE_ID_EXTRA_KEY, 0);
             model.setArticleId(articleId);
+
+            model.getArticleLiveData().observe(this, new Observer<Article>() {
+                @Override
+                public void onChanged(@Nullable Article article) {
+                    if (article != null) {
+                        binding.setArticle(article);
+                    }
+                }
+            });
         } else {
             finish();
         }
