152460b85376eda85a5e6f91ab4bda0f8905f42b
Anderson Brandao <anderson.brands@gmail.com>
2019-02-13 21:56:23 -0200

    feat: Make setImageURL bindingadapter params not required

--- app/src/main/java/com/adnd/xyzreader/adapters/BindingAdapters.java
+++ app/src/main/java/com/adnd/xyzreader/adapters/BindingAdapters.java
@@ -7,19 +7,23 @@ import android.widget.ImageView;
 
 import com.adnd.xyzreader.custom_views.AspectRatioImageView;
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.RequestCreator;
 
 public class BindingAdapters {
 
-    @BindingAdapter(value = {"imgURL", "placeholderRes", "errorRes"})
+    @BindingAdapter(value = {"imgURL", "placeholderRes", "errorRes"}, requireAll = false)
     public static void setImgURL(ImageView imageView, String imgURL, Drawable placeholderRes, Drawable errorRes) {
         if (TextUtils.isEmpty(imgURL)) {
             imgURL = "Path must not be empty";
         }
-        Picasso.get()
-                .load(imgURL)
-                .placeholder(placeholderRes)
-                .error(errorRes)
-                .into(imageView);
+        RequestCreator requestCreator = Picasso.get().load(imgURL);
+        if (placeholderRes != null) {
+            requestCreator.placeholder(placeholderRes);
+        }
+        if (errorRes != null) {
+            requestCreator.error(errorRes);
+        }
+        requestCreator.into(imageView);
     }
 
     @BindingAdapter("aspectRatio")
