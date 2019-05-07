7ba05ccff131eafe061db339a07f2981db7a15b2
Anderson Brandao <anderson.brands@gmail.com>
2019-05-07 16:18:14 -0300

    Include accounts list fragment into main activity

--- app/src/main/java/com/adnd/iomoney/MainActivity.java
+++ app/src/main/java/com/adnd/iomoney/MainActivity.java
@@ -3,11 +3,19 @@ package com.adnd.iomoney;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.adnd.iomoney.adapters.ListItemClickListener;
+import com.adnd.iomoney.models.Account;
+
+public class MainActivity extends AppCompatActivity implements ListItemClickListener<Account> {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onListItemClick(Account clickedItem, int position) {
+        // TODO handle click
     }
 }
