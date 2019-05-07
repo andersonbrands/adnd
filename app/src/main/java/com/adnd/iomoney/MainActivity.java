e7d225a8d390bf42f170e86f413b38d3b4746e36
Anderson Brandao <anderson.brands@gmail.com>
2019-05-07 17:23:48 -0300

    Add floating action button to main activity

--- app/src/main/java/com/adnd/iomoney/MainActivity.java
+++ app/src/main/java/com/adnd/iomoney/MainActivity.java
@@ -1,7 +1,11 @@
 package com.adnd.iomoney;
 
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 import com.adnd.iomoney.adapters.ListItemClickListener;
 import com.adnd.iomoney.models.Account;
@@ -12,6 +16,20 @@ public class MainActivity extends AppCompatActivity implements ListItemClickList
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        toolbar.setTitle(R.string.app_name);
+
+        FloatingActionButton fab = findViewById(R.id.fab_add_account);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
     }
 
     @Override
