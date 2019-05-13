113a966a6b13e6646dad4ceb890a3a618f2575ec
Anderson Brandao <anderson.brands@gmail.com>
2019-05-13 13:25:14 -0300

    Add transaction with adapter

---
+++ app/src/main/java/com/adnd/iomoney/adapters/TransactionAdapter.java
@@ -0,0 +1,57 @@
+package com.adnd.iomoney.adapters;
+
+import android.databinding.DataBindingUtil;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.adnd.iomoney.R;
+import com.adnd.iomoney.databinding.TransactionListItemBinding;
+import com.adnd.iomoney.databinding.TransactionListItemBinding;
+import com.adnd.iomoney.models.Account;
+import com.adnd.iomoney.models.Transaction;
+
+import java.util.List;
+
+public class TransactionAdapter extends BaseAdapter<Transaction, TransactionAdapter.TransactionViewHolder> {
+
+    final private ListItemClickListener<Transaction> mListItemClickListener;
+
+    public TransactionAdapter(List<Transaction> objects, ListItemClickListener<Transaction> mListItemClickListener) {
+        super(objects);
+        this.mListItemClickListener = mListItemClickListener;
+    }
+
+    @NonNull
+    @Override
+    public TransactionViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        TransactionListItemBinding binding =
+                DataBindingUtil.inflate(inflater, R.layout.transaction_list_item, parent, false);
+        return new TransactionViewHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TransactionViewHolder accountViewHolder, int position) {
+        Transaction transaction = getObjects().get(position);
+        accountViewHolder.binding.setDescription(transaction.getDescription());
+    }
+
+    class TransactionViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TransactionListItemBinding binding;
+
+        TransactionViewHolder(@NonNull TransactionListItemBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
+            binding.getRoot().setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            final int clickedPosition = getAdapterPosition();
+            mListItemClickListener.onListItemClick(getObjects().get(clickedPosition), clickedPosition);
+        }
+    }
+}
---
+++ app/src/main/java/com/adnd/iomoney/models/Transaction.java
@@ -0,0 +1,107 @@
+package com.adnd.iomoney.models;
+
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.ForeignKey;
+import android.arch.persistence.room.Index;
+import android.arch.persistence.room.PrimaryKey;
+
+@Entity(tableName = "transactions",
+        foreignKeys = @ForeignKey(
+                entity = Account.class,
+                parentColumns = "id",
+                childColumns = "account_id"
+        )
+)
+public class Transaction {
+
+    private int account_id;
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    private String description;
+
+    private float value;
+
+    private String tags;
+
+    private boolean hasLocation;
+
+    private String locationLabel;
+
+    private long lat;
+
+    private long lon;
+
+    public int getAccount_id() {
+        return account_id;
+    }
+
+    public void setAccount_id(int account_id) {
+        this.account_id = account_id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public float getValue() {
+        return value;
+    }
+
+    public void setValue(float value) {
+        this.value = value;
+    }
+
+    public String getTags() {
+        return tags;
+    }
+
+    public void setTags(String tags) {
+        this.tags = tags;
+    }
+
+    public boolean isHasLocation() {
+        return hasLocation;
+    }
+
+    public void setHasLocation(boolean hasLocation) {
+        this.hasLocation = hasLocation;
+    }
+
+    public String getLocationLabel() {
+        return locationLabel;
+    }
+
+    public void setLocationLabel(String locationLabel) {
+        this.locationLabel = locationLabel;
+    }
+
+    public long getLat() {
+        return lat;
+    }
+
+    public void setLat(long lat) {
+        this.lat = lat;
+    }
+
+    public long getLon() {
+        return lon;
+    }
+
+    public void setLon(long lon) {
+        this.lon = lon;
+    }
+}
