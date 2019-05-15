96f413fbc1a5b8fd1a274fb3fa06bd3356076fed
Anderson Brandao <anderson.brands@gmail.com>
2019-05-15 18:05:33 -0300

    Add accounts list to AddEditTransactionViewModel

--- app/src/main/java/com/adnd/iomoney/view_models/AddEditTransactionViewModel.java
+++ app/src/main/java/com/adnd/iomoney/view_models/AddEditTransactionViewModel.java
@@ -10,22 +10,28 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.widget.DatePicker;
 
+import com.adnd.iomoney.models.Account;
 import com.adnd.iomoney.models.Transaction;
+import com.adnd.iomoney.repositories.AccountsRepository;
 import com.adnd.iomoney.repositories.TransactionsRepository;
 import com.adnd.iomoney.utils.OperationResult;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 public class AddEditTransactionViewModel extends AndroidViewModel implements DatePickerDialog.OnDateSetListener {
 
     private MediatorLiveData<Transaction> transactionLiveData = new MediatorLiveData<>();
+    private MediatorLiveData<List<Account>> accountsLiveData = new MediatorLiveData<>();
+    private AccountsRepository accountsRepository;
     private TransactionsRepository transactionsRepository;
 
     public AddEditTransactionViewModel(@NonNull Application application) {
         super(application);
 
         transactionsRepository = new TransactionsRepository(application);
+        accountsRepository = new AccountsRepository(application);
     }
 
     public void setup(int transaction_id, final int account_id) {
@@ -47,10 +53,25 @@ public class AddEditTransactionViewModel extends AndroidViewModel implements Dat
                 transactionLiveData.removeSource(source);
             }
         });
+        loadAccounts();
     }
 
     public LiveData<OperationResult> saveTransaction() {
         return transactionsRepository.addTransaction(transactionLiveData.getValue());
+    }
+
+    public void loadAccounts() {
+        final LiveData<List<Account>> source = accountsRepository.loadAccounts();
+        accountsLiveData.addSource(source, new Observer<List<Account>>() {
+            @Override
+            public void onChanged(@Nullable List<Account> accounts) {
+                accountsLiveData.setValue(accounts);
+            }
+        });
+    }
+
+    public MediatorLiveData<List<Account>> getAccountsLiveData() {
+        return accountsLiveData;
     }
 
     public MediatorLiveData<Transaction> getTransactionLiveData() {
