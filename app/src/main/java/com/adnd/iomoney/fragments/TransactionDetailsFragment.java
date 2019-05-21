9d14985d8da69ebf68a2a35350390c684de71732
Anderson Brandao <anderson.brands@gmail.com>
2019-05-20 21:51:15 -0300

    Show account name instead of id; open location in map

--- app/src/main/java/com/adnd/iomoney/fragments/TransactionDetailsFragment.java
+++ app/src/main/java/com/adnd/iomoney/fragments/TransactionDetailsFragment.java
@@ -2,6 +2,8 @@ package com.adnd.iomoney.fragments;
 
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -36,24 +38,37 @@ public class TransactionDetailsFragment extends Fragment {
         if (getActivity() != null) {
             model = ViewModelProviders.of(getActivity()).get(TransactionViewModel.class);
 
+            model.getAccountsLiveData().observe(getActivity(), new Observer<List<Account>>() {
+                @Override
+                public void onChanged(@Nullable List<Account> accountList) {
+                    TransactionDetailsFragment.this.accountList = accountList;
+                }
+            });
+
             model.getTransactionLiveData().observe(getActivity(), new Observer<Transaction>() {
                 @Override
                 public void onChanged(@Nullable Transaction transaction) {
                     binding.setTransaction(transaction);
 
-                    for (Account account : accountList) {
-                        if (account.getId() == transaction.getAccount_id()) {
-                            binding.setAcountName(account.getName());
-                            break;
+                    if (accountList != null) {
+                        for (Account account : accountList) {
+                            if (account.getId() == transaction.getAccount_id()) {
+                                binding.setAcountName(account.getName());
+                                break;
+                            }
                         }
                     }
                 }
             });
 
-            model.getAccountsLiveData().observe(getActivity(), new Observer<List<Account>>() {
+
+            binding.btShowInMap.setOnClickListener(new View.OnClickListener() {
                 @Override
-                public void onChanged(@Nullable List<Account> accountList) {
-                    TransactionDetailsFragment.this.accountList = accountList;
+                public void onClick(View v) {
+                    Transaction t = binding.getTransaction();
+                    Uri uri = Uri.parse(String.format("geo:<%f>,<%f>", t.getLat(), t.getLon()));
+                    Intent intent = new Intent(android.content.Intent.ACTION_VIEW, uri);
+                    startActivity(intent);
                 }
             });
         }
